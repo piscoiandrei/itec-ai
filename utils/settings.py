f36c2182c3089d109301fbde25930f122fe186e6
Piscoi Andrei <piscoiandrei27@gmail.com>
2022-05-28 11:53:06 +0300

    Generating images, all from python, no optimization

---
+++ utils/settings.py
@@ -0,0 +1,22 @@
+zone = 10
+TRIANGLE = "triangle"
+RECTANGLE = "rectangle"
+ELLIPSE = "ellipse"
+SHAPES = [TRIANGLE, RECTANGLE, ELLIPSE]
+COLORS = {
+    "red": "#ff0000",
+    "green": "#00ff00",
+    "blue": "#0000ff"
+}
+SHAPE_CODES = {
+    TRIANGLE: 0,
+    ELLIPSE: 1,
+    RECTANGLE: 2
+}
+COLOR_CODES = {
+    "red": 0,
+    "green": 1,
+    "blue": 2
+}
+SIZE = 96
+TOTAL_AREA = SIZE ** 2
