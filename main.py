03c29d2282af2cda1b7dfb6d70f79eb8be3510b4
Piscoi Andrei <piscoiandrei27@gmail.com>
2022-05-29 00:37:15 +0300

    Multiprocessing for k-mean algorithm

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 import argparse
-import timeit
+import time
 from utils.generate import generate
-from utils.kmeans import compute_kmeans
+from utils.kmeans import kmean_processes
 
 if __name__ == '__main__':
     if not os.path.isdir('images/'):
-        imports = 'from __main__ import generate'
-        elapsed = timeit.timeit('generate()', imports, number=1)
-        print(f'Generated images in {elapsed:.2f}s')
-    kmean_time = timeit.timeit('compute_kmeans()',
-                               'from __main__ import compute_kmeans'
-                               , number=1)
-    print(f'Applied K-mean in {kmean_time:.2f}s')
+        generate()
+    start = time.time()
+    kmean_processes()
+    end = time.time()
+    print(end - start)
--- utils/kmeans.py
+++ utils/kmeans.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import cv2
 import numpy as np
+import multiprocessing as mp
 from PIL import Image
 
 from utils.settings import TOTAL_AREA
@@ -38,32 +39,53 @@ def get_color_and_area(img):
     )] / TOTAL_AREA
 
 
-def compute_kmeans():
+def compute_kmeans(path):
     init()
-    paths = files = os.listdir('images/')
+    image = cv2.imread(f'images/{path}')
+    # convert to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # reshape the image to a 2D array of pixels and 3 color values (RGB)
+    pixel_values = image.reshape((-1, 3))
+    # convert to float
+    pixel_values = np.float32(pixel_values)
+    # define stopping criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.2)
+    # number of clusters (K)
+    k = 2
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10,
+                                      cv2.KMEANS_RANDOM_CENTERS)
+    # convert back to 8 bit values
+    centers = np.uint8(centers)
+    # flatten the labels array
+    labels = labels.flatten()
+    # convert all pixels to the color of the centroids
+    segmented_image = centers[labels.flatten()]
+    # reshape back to the original image dimension
+    segmented_image = segmented_image.reshape(image.shape)
+    color, area = get_color_and_area(segmented_image)
+    return ",".join(str(x) for x in [path, color, area])
+
+
+def kmeans_range(final_data, paths):
     data = []
-    for path in paths:
-        image = cv2.imread(f'images/{path}')
-        # convert to RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # reshape the image to a 2D array of pixels and 3 color values (RGB)
-        pixel_values = image.reshape((-1, 3))
-        # convert to float
-        pixel_values = np.float32(pixel_values)
-        # define stopping criteria
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.2)
-        # number of clusters (K)
-        k = 2
-        _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10,
-                                          cv2.KMEANS_RANDOM_CENTERS)
-        # convert back to 8 bit values
-        centers = np.uint8(centers)
-        # flatten the labels array
-        labels = labels.flatten()
-        # convert all pixels to the color of the centroids
-        segmented_image = centers[labels.flatten()]
-        # reshape back to the original image dimension
-        segmented_image = segmented_image.reshape(image.shape)
-        color, area = get_color_and_area(segmented_image)
-        data.append((path, color, area))
-    return data
+    for p in paths:
+        data.append(compute_kmeans(p))
+    final_data.append(data)
+
+
+def kmean_processes():
+    manager = mp.Manager()
+    final_data = manager.list()
+    paths = files = os.listdir('images/')
+    processes = []
+    for i in range(0, len(paths), 100):
+        proc = mp.Process(target=kmeans_range,
+                          args=(final_data, paths[i:i + 100],))
+        processes.append(proc)
+        proc.start()
+    for p in processes:
+        p.join()
+    final_data = [s for sublist in final_data for s in sublist]
+    with open('kmean_out.txt', 'w') as f:
+        for x in final_data:
+            f.writelines(x + "\n")
